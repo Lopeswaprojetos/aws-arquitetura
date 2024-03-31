06f6961dec09d20eb856b9b8f79972de5fe8fd36
Wagner Lopes <161225187+Lopeswaprojetos@users.noreply.github.com>
2024-03-31 11:31:26 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+User
 terraform {
   required_providers {
     aws = {
@@ -12,18 +13,18 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr_block
 }
 
 resource "aws_subnet" "public_subnet_a" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = var.subnet_a_cidr_block
   availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "public_subnet_b" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = var.subnet_b_cidr_block
   availability_zone = "us-east-1b"
 }
 
@@ -31,33 +32,44 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_vpc_attachment" "gw_attach" {
-  vpc_id       = aws_vpc.main.id
-  internet_gateway_id = aws_internet_gateway.gw.id
-}
-
 resource "aws_elb" "web-lb" {
   name               = "web-lb"
   availability_zones = ["us-east-1a", "us-east-1b"]
-  subnets            = [aws_subnet.public_subnet_a.id, aws_subnet.public_subnet_b.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "HTTP"
+    lb_port           = 80
+    lb_protocol       = "HTTP"
+  }
+
+  subnets = [aws_subnet.public_subnet_a.id, aws_subnet.public_subnet_b.id]
 }
 
 resource "aws_launch_configuration" "data-processing-lc" {
-  image_id        = "ami-123456"
-  instance_type   = "t2.micro"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
 }
 
 resource "aws_autoscaling_group" "data-processing-asg" {
-  launch_configuration = aws_launch_configuration.examplo.id
+  launch_configuration = aws_launch_configuration.data-processing-lc.id
   min_size             = 2
   max_size             = 10
   desired_capacity     = 2
   vpc_zone_identifier  = [aws_subnet.public_subnet_a.id, aws_subnet.public_subnet_b.id]
 }
 
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
 resource "aws_nat_gateway" "teste-nat-gateway" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public_subnet_a.id
+
+  tags = {
+    Name = "MyNatGateway"
+  }
 }
 
 resource "aws_s3_bucket" "my-test-bucket" {
@@ -70,8 +82,18 @@ resource "aws_route53_zone" "teste-exemplo-zone" {
 
 resource "aws_cloudfront_distribution" "static-website-distribution" {
   origin {
-    domain_name = aws_s3_bucket.my_bucket.website_endpoint
+    domain_name = aws_s3_bucket.my-test-bucket.bucket_regional_domain_name
     origin_id   = "S3-my-bucket"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
   }
 
   enabled             = true
@@ -92,52 +114,36 @@ resource "aws_cloudfront_distribution" "static-website-distribution" {
       }
     }
   }
-} 
+}
 
-resource "aws_waf_web_acl" "web-security-acl" {
-  name        = "web-security-acl"
-  metric_name = "web-security-acl"
+resource "aws_waf_web_acl" "another-web-security-acl" {
+  name        = "new-web-security-acl"
+  metric_name = "new_web_security_acl_metric123"  # Corrigido para conter apenas caracteres alfanuméricos
 
   default_action {
     type = "ALLOW"
   }
 
-
-rule {
-    name        = "block-all"
-    priority    = 1
-    action      = "BLOCK"
-    override_action = {
-      type = "NONE"
-    }
-
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesCommonRuleSet"
-        vendor_name = "AWS"
-      }
-    }
-  }
+  # Adicione suas regras aqui conforme necessário
 }
 
 resource "aws_vpc_endpoint" "dynamodb-endpoint" {
-  vpc_id = aws_vpc.main.id
-
+  vpc_id       = aws_vpc.main.id
   service_name = "com.amazonaws.us-east-1.dynamodb"
 }
 
 resource "aws_cloudwatch_metric_alarm" "ec2-cpu_utilization_alarm" {
   alarm_name          = "ec2-cpu_utilization_alarm"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods = 2
-  metric_name        = "CPUUtilization_alarm"
-  namespace          = "AWS/EC2"
-  period             = 120
-  statistic          = "Average"
-  threshold          = 80
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization_alarm"
+  namespace           = "AWS/EC2"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 80
 
-  dimensions {
-    AutoScalingGroupName = aws_autoscaling_group.example.name
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.data-processing-asg.name
   }
 
   alarm_description = "This metric monitors EC2 instance CPU utilization"
