f1e7605d617773da66fd03250bf8019baa18fc30
Wagner Lopes <161225187+Lopeswaprojetos@users.noreply.github.com>
2024-03-31 11:33:08 -0300

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,11 +1,29 @@
-variable "region" {
-  default = "us-east-1"
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_a_cidr_block" {
+  description = "CIDR block for public subnet A"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "subnet_b_cidr_block" {
+  description = "CIDR block for public subnet B"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "instance_type" {
+  description = "Instance type for the launch configuration"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "ami_id" {
-  default = "ami-0e731c8a588258d0d"
-}
-
-variable "vpc_id" {
-  default = "vpc-f1639e8c"
+  description = "AMI ID for the launch configuration"
+  type        = string
+  default     = "ami-123456"
 }
